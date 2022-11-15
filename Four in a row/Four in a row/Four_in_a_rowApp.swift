d4b63a902c3e5e30a53345279e5ce43a4fd19ab6
Vitalie Lopatiuc <vlopatiuc@gmail.com>
2022-11-14 22:01:54 -0500

    First

---
+++ Four in a row/Four in a row/Four_in_a_rowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Four_in_a_rowApp.swift
+//  Four in a row
+//
+//  Created by Vitalie Lopatiuc on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Four_in_a_rowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
